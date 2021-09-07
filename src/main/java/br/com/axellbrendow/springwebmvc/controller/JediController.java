ef1f507d143cb9a4af769ece8371defb7e331d57
axell-brendow <breno.axel@gmail.com>
2021-09-07 20:12:49 -0300

    Create Jedi model and return ModelAndView on /jedi

--- src/main/java/br/com/axellbrendow/springwebmvc/controller/JediController.java
+++ src/main/java/br/com/axellbrendow/springwebmvc/controller/JediController.java
@@ -1,12 +1,20 @@
 package br.com.axellbrendow.springwebmvc.controller;
 
+import br.com.axellbrendow.springwebmvc.model.Jedi;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class JediController {
     @GetMapping("/jedi")
-    public String jedi() {
-        return "jedi"; // same name as resources/templates/jedi.html
+    public ModelAndView jedi() {
+        final var modelAndView = new ModelAndView();
+        modelAndView.setViewName("jedi");
+        final var luke = new Jedi("Luke", "Skywalker");
+        modelAndView.addObject("allJedi", List.of(luke));
+        return modelAndView;
     }
 }
---
+++ src/main/java/br/com/axellbrendow/springwebmvc/model/Jedi.java
@@ -0,0 +1,29 @@
+package br.com.axellbrendow.springwebmvc.model;
+
+public class Jedi {
+    private String name;
+    private String lastName;
+
+    public Jedi(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Jedi() { }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
